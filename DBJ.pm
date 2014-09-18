bc642fd209b0b14c43679a11bd5a2913f463fa4c
dokechin <ttatsumi@ra2.so-net.ne.jp>
2014-09-18 08:53:38 +0000

    add plugin sample

---
+++ DBJ.pm
@@ -0,0 +1,4 @@
+package DBJ;
+use parent 'Teng';
+__PACKAGE__->load_plugin('SearchJoined');
+1;
--- test.pl
+++ test.pl
@@ -1,19 +1,40 @@
 use strict;
 use warnings;
 use DB;
+use DBJ;
 
+# This sample is bad case. Joined result inflate is not called.
 my $teng = DB->new(+{connect_info => ['dbi:SQLite:./profile','','']});
 
-my @rows = $teng->search_by_sql("select user.id,food.name from user left join food on user.food_id = food.id",[],"food");
+my @rows = $teng->search_by_sql("select user.id,food.name from user left join food on user.food_id = food.id",[]);
 
 for my $row(@rows){
   warn $row->id;
   warn $row->name;
 }
 
-my @rows = $teng->search_by_sql("select user.id,food.name from user left join food on user.food_id = food.id",[],"food");
+# This sample is bad case. Joined result inflate is not called.
 
-for my $row(@rows){
-  warn $row->id;
-  warn $row->name;
+my $ite = DB->new(+{connect_info => ['dbi:SQLite:./profile','','']});
+
+my $ite = $teng->search_by_sql("select * from user left join food on user.food_id = food.id",[]);
+
+while (my ($joined) = $ite->next) {
+  warn $joined->id;
+  warn $joined->name;
 }
+
+
+# This sample is good case. Joined table "food" inflate is called.
+my $tengj = DBJ->new(+{connect_info => ['dbi:SQLite:./profile','','']});
+
+$ite = $tengj->search_joined(user => [
+               food => {'user.food_id' => 'food.id'},
+           ]);
+
+while (my ($user, $food) = $ite->next) {
+  warn $user->id;
+  warn $food->name;
+}
+
+
